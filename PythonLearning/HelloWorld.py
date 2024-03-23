8c079539ef1a39650b2c500db60dfb566fde6ba1
DevPupsLayks <a.paramonov13@outlook.com>
2024-03-24 02:35:36 +0300

    +

--- PythonLearning/HelloWorld.py
+++ PythonLearning/HelloWorld.py
@@ -1,5 +1,7 @@
-x = 99
-print(x)
-x = ("Отведите меня в номер")
-print(x)
+## 9bottles = 1
+july2009 = 2
+theSalesFigureForFiscalYear = 3
+## r&d = 4
+grade_report = 5
 
+print(july2009, theSalesFigureForFiscalYear, grade_report)
