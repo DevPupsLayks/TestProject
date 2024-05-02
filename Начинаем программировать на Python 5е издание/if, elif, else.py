1faca07e0f603193cec264d6f9472ce5c6c34389
DevPupsLayks <a.paramonov13@outlook.com>
2024-05-02 16:49:36 +0300

    +

---
+++ Начинаем программировать на Python 5е издание/if, elif, else.py
@@ -0,0 +1,12 @@
+time = 8
+if time <= 5:
+    print("Добрый ночи")
+elif time <= 9:
+    print("Доброе утро")
+elif time <= 18:
+    print("Добрый день")
+elif time <= 21:
+    print("Добрый вечер")
+elif time <= 24:
+    print("Добрый ночи")
+else: print("Ошибка формата времени 24ч")
---
+++ Начинаем программировать на Python 5е издание/new.py
@@ -0,0 +1,4 @@
+a = 1000000
+while a >= 1:
+    print(a)
+    a = a + 1
