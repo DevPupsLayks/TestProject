81410e7d10b086e7dcddbaab426d84918c2b1248
DevPupsLayks <a.paramonov13@outlook.com>
2024-05-06 12:01:14 +0300

    Корректировка

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -3,7 +3,9 @@ import telebot
 bot = telebot.TeleBot('7162762804:AAFpHcd57Ns6KjyEWnAwWTqsIM3eSLjsq9k')
 
 @bot.message_handler(content_types=['text'])
+
 def handle_text(message):
+
     if message.text.lower() == "привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text.lower() == "/help":
@@ -12,8 +14,8 @@ def handle_text(message):
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 @bot.message_handler(content_types=['document', 'audio'])
+
 def handle_other(message):
-    # Обработка других типов контента
     pass
 
 if __name__ == "__main__":
