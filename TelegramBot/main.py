efc7dbeeeb8e536a1f4e0cac4f82bbfc513de770
DevPupsLayks <a.paramonov13@outlook.com>
2024-05-06 11:59:19 +0300

    Добавление функционала

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -1,10 +1,20 @@
-import asyncio
-from telegram import Bot
+import telebot
 
-async def send_message():
-    TOKEN = '7162762804:AAFpHcd57Ns6KjyEWnAwWTqsIM3eSLjsq9k'
-    bot = Bot(TOKEN)
-    chat_id = 797676862  # Numeric chat ID
-    await bot.send_message(chat_id=chat_id, text='Hello World!')
+bot = telebot.TeleBot('7162762804:AAFpHcd57Ns6KjyEWnAwWTqsIM3eSLjsq9k')
 
-asyncio.run(send_message())
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text.lower() == "привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text.lower() == "/help":
+        bot.send_message(message.from_user.id, "Напиши Привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_other(message):
+    # Обработка других типов контента
+    pass
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True, interval=0)
