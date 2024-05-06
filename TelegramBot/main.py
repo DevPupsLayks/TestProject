ff969d60278e820e87e40ad0d8692af8c7a0e950
DevPupsLayks <a.paramonov13@outlook.com>
2024-05-06 09:23:48 +0300

    Добавление бота + проверка работоспособности

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -1,4 +1,10 @@
-import time
-for i in range(50):
-    time.sleep(2)
-    print("Hello")
+import asyncio
+from telegram import Bot
+
+async def send_message():
+    TOKEN = '7162762804:AAFpHcd57Ns6KjyEWnAwWTqsIM3eSLjsq9k'
+    bot = Bot(TOKEN)
+    chat_id = 797676862  # Numeric chat ID
+    await bot.send_message(chat_id=chat_id, text='Hello World!')
+
+asyncio.run(send_message())
