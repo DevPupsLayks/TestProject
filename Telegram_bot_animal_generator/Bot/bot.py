1c6ab19fa80cea1840e9626fa89fbbac0fd63aaf
DevPupsLayks <a.paramonov13@outlook.com>
2024-05-13 07:43:14 +0300

    Hot FIX

--- Telegram_bot_animal_generator/Bot/bot.py
+++ Telegram_bot_animal_generator/Bot/bot.py
@@ -1,12 +1,12 @@
-from telegram import Update
+from telegram import Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from neural_network.animal_generator import generate_animal_image
 # Функция для обработки команды /start
-def start(update: Update, context: CallbackContext) -> None:
+def start(update: Bot, context: CallbackContext) -> None:
     update.message.reply_text('Привет! Я бот, который генерирует картинки с животными. Введите /generate, чтобы получить картинку.')
 
 # Функция для обработки команды /generate
-def generate(update: Update, context: CallbackContext) -> None:
+def generate(update: Bot, context: CallbackContext) -> None:
     # Генерируем изображение с животным
     animal_image = generate_animal_image()
     # Отправляем изображение пользователю
@@ -15,8 +15,8 @@ def generate(update: Update, context: CallbackContext) -> None:
 
 # Функция для запуска бота
 def main() -> None:
-    # Замените 'YOUR_TOKEN' на ваш токен Telegram бота
-    updater = Updater(token="7162762804:AAFpHcd57Ns6KjyEWnAwWTqsIM3eSLjsq9k")
+    token = "7162762804:AAFpHcd57Ns6KjyEWnAwWTqsIM3eSLjsq9k"  # Получение токена из переменной окружения
+    updater = Updater.from_token(token)
     dispatcher = updater.dispatcher
 
     # Добавляем обработчики команд
@@ -28,4 +28,4 @@ def main() -> None:
     updater.idle()
 
 if __name__ == '__main__':
-    main()
+    bot_main()
